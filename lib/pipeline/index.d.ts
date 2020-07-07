f2381bbe2a1d8cdb1089a7ca6596d31f1959247d
Chris J. Avery <c.avery1989@gmail.com>
2020-07-07 20:21:38 +0100

    prevent test getting published

--- lib/pipeline/index.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="hapi__joi" />
-import { Middleware, APIGatewayHandler } from '../types';
-import { ObjectSchema } from '@hapi/joi';
-export declare const buildPipeline: (middleware: Middleware[]) => {
-    getHandler: <TOut>(api: any) => APIGatewayHandler;
-    postHandler: <TIn, TOut_1>(api: any, schema: ObjectSchema<TIn>) => APIGatewayHandler;
-};
-//# sourceMappingURL=index.d.ts.map
--- lib/types.d.ts
+++
@@ -1,5 +0,0 @@
-import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
-export declare type APIGatewayHandler = (event: APIGatewayProxyEventV2) => Promise<APIGatewayProxyStructuredResultV2>;
-export declare type Middleware = (next: APIGatewayHandler) => APIGatewayHandler;
-export declare type MiddlewareFactory<T> = (config?: T) => Middleware;
-//# sourceMappingURL=types.d.ts.map
