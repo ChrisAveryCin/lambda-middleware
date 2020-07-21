31b6636c2499d19c5d446c49a1e4ab1d1a17a43b
ChrisAveryCin <66006071+ChrisAveryCin@users.noreply.github.com>
2020-07-21 10:53:46 +0100

    Update src/types.ts
    Co-authored-by: Adrian Hesketh <a-h@users.noreply.github.com>

--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,5 @@
 import { APIGatewayProxyEventV2, APIGatewayProxyStructuredResultV2 } from 'aws-lambda';
 
 export type APIGatewayHandler = (event: APIGatewayProxyEventV2) => Promise<APIGatewayProxyStructuredResultV2>;
-export type Middleware = (next: APIGatewayHandler) => APIGatewayHandler
+export type Middleware = (next: APIGatewayHandler) => APIGatewayHandler;
 export type MiddlewareFactory<T> = (config?: T) => Middleware
-
